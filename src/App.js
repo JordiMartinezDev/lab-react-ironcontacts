090011d10abb54924e67ca5833c96c31bfb12b96
Jordi Martinez <jordiupc87@gmail.com>
2022-10-25 18:01:37 +0200

    Iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import { useState } from "react";
+import "./App.css";
+import Data from "./contacts.json";
+
+const contacts = Data.slice(0, 5);
+console.log(contacts);
 
 function App() {
+  const [updateContacts, setUpdateContacts] = useState(contacts);
+
+  function addRandomContact() {
+    const newArray = updateContacts.map((contact) => {
+      return contact;
+    });
+
+    let randomNum = ~~(Math.random() * Data.length);
+
+    newArray.push(Data[randomNum]);
+    Data.splice(randomNum, 1);
+    setUpdateContacts(newArray);
+    console.log(Data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> IronContacts</h1>
+      <button onClick={addRandomContact}>Contact</button>
+      <table>
+        <tr>
+          <td>Picture</td>
+          <td>Name</td>
+          <td>Popularity</td>
+          <td>Won Oscar</td>
+          <td>Won Emmy</td>
+        </tr>
+        {updateContacts.map((contact) => {
+          return (
+            <tr>
+              <td>
+                <img src={contact.pictureUrl} width="150" height="200" />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>{contact.wonOscar ? "🏆" : ""}</td>
+              <td>{contact.wonEmmy ? " 🏆" : ""}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
-
 export default App;
